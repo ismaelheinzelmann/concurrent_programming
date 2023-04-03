c27fa8c5cb9a40971ebd1aea5b86a1885d81f5eb
Ismael Heinzelmann <ismaelheinzelmann@gmail.com>
2023-04-03 17:42:09 -0300

    logic reverse

--- af_2_1_processos/exercicio_2/main.c
+++ af_2_1_processos/exercicio_2/main.c
@@ -23,83 +23,46 @@
 // - netos devem esperar 5 segundos antes de imprmir a mensagem de finalizado (e terminar)
 // - pais devem esperar pelos seu descendentes diretos antes de terminar
 
-int child()
-{
-    pid_t grandChild1, grandChild2, grandChild3;
-    grandChild1 = fork();
-    if (grandChild1 != 0)
-    {
-        grandChild2 = fork();
-        if (grandChild2 != 0)
-        {
-            grandChild3 = fork();
-        }
-    }
-
-    if (grandChild1 >= 0 && grandChild2 >= 0 && grandChild3 >= 0)
-    {
-        if (grandChild1 == 0 || grandChild2 == 0 || grandChild3 == 0)
-        {
-            printf("Processo %d, filho de %d\n", getpid(), getppid());
-            sleep(5);
-            printf("Processo %d finalizado\n", getpid());
-            return 0;
-        }
-        else
-        {
-            while (wait(NULL) >= 0)
-            {
-                ;
-            }
-        }
-    }
-    else
-    {
-        printf("Erro ao criar processo.\n");
-        return 1;
-    }
-    return 0;
-}
-
 int main(int argc, char **argv)
 {
-    pid_t child1, child2;
-    child1 = fork();
-    if (child1 != 0)
-    {
-        child2 = fork();
-    }
+    pid_t childs[2];
+    pid_t fatherPid = getpid();
 
-    if (child1 >= 0 || child2 >= 0)
+    for (size_t i = 0; i < 2; i++)
     {
+        childs[i] = fork();
 
-        if (child1 == 0 || child2 == 0)
+        if (childs[i] == 0)
         {
             printf("Processo %d, filho de %d\n", getpid(), getppid());
-            int childProcess = child();
-            if (childProcess != 0)
+            for (int i = 0; i < 3; i++)
             {
-                return childProcess;
+                pid_t grandChild = fork();
+
+                if (grandChild == 0){
+                    printf("Processo %d, filho de %d\n", getpid(), getppid());
+                    sleep(5);
+                    break;
+                }
             }
-            else if (childProcess > 0)
-            {
-                printf("Processo %d finalizado\n", getpid());
-            }
-            return 0;
+            
+            break;
         }
-        else
-        {
-            while (wait(NULL) >= 0)
-            {
-                ;
-            }
-        }
+    }
+
+    while (wait(NULL) >= 0)
+    {
+        ;
+    }
+
+    if (getpid() == fatherPid)
+    {
+        printf("Processo principal %d finalizado\n", fatherPid);
     }
     else
     {
-        printf("Erro ao criar processos.\n");
-        return 1;
+        printf("Processo %d finalizado\n", getpid());
     }
-    printf("Processo principal %d finalizado\n", getpid());
+
     return 0;
 }
